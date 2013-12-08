a9ba5b1bb242b708db56dda5c189390061f41ebf
Devesh Chanchlani <dchanchlani@solutionsiq.com>
2013-12-08 17:43:23 +0530

    completing the post-answer default loop

--- libs/survey.js
+++ libs/survey.js
@@ -7,5 +7,8 @@ exports.questions =
  	{
  		query: "One word for the trainer",
  		type: questionTypeEnum.text
+ 	}, {
+ 		query: "Few Paras for the trainer",
+ 		type: questionTypeEnum.multiLine
  	}
 ];
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,26 @@ exports.index = function(req, res){
 var questionIndex = 0;
 
 exports.getFeedBack = function(req, res) {
+  console.log("index=" + questionIndex);
   var question = survey.questions[questionIndex];
-  var viewToRender = 'feedmeback-' + question.type.id;
-  res.render(viewToRender, { question: question, index: questionIndex });
+  if(question !== undefined) {
+	  var viewToRender = 'feedmeback-' + question.type.id;
+	  res.render(viewToRender, { question: question, index: questionIndex });
+  } else {
+	  res.render("thankyou", {});
+  }
+};
+
+exports.postAnswer = function() {
+	return function(req, res) {
+		//TODO: record answer
+		console.log("Question Answered = " + req.body.qNo);
+		console.log("Feedback Given = " + req.body.answer);
+		
+		questionIndex++;
+		// If it worked, set the header so the address bar doesn't still say /adduser
+        res.location("feedmeback");
+        // And forward to success page
+        res.redirect("feedmeback");
+	};
 };
